7e6cc1ec755e398f5a570f47f1835909f67544f7
clayton-snyder <snyderc@gmx.com>
2021-04-26 23:47:20 -0700

    Random numbers, and print out channel IDs and names

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
     "os"
     "os/signal"
     "syscall"
+    "math/rand"
 
     "github.com/bwmarrin/discordgo"
 )
@@ -45,7 +46,21 @@ func main() {
     /* WHAT IS THIS */
     /* does `<-sc` make the code wait for the signal.Notify() before it? That would be cool!!!!! */
     // Wait here until CTRL-C or other term signal is received.
+    /*
     fmt.Println("Bot runnin'. ^C to exit.")
+    for _, guild := range dg.State.Guilds {
+        channels, _ := dg.GuildChannels(guild.ID)
+        for _, c := range channels {
+            // Check if channel is a guild text channel and not a voice or DM channel
+            if c.Type != discordgo.ChannelTypeGuildText {
+                continue
+            }
+            fmt.Println("cid %d and name %q", c.ID, c.Name)
+
+        }
+    }*/
+//    dg.ChannelMessageSend();
+//252302649884409859 
     sc := make(chan os.Signal, 1)
     signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
     <-sc
@@ -63,12 +78,27 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
         return
     }
 
+    fmt.Println(m.Author.ID);
+    fmt.Println(m.Author.Email);
+    fmt.Println(m.Author.Username);
+
     // If the message is "ping" reply with "pang"
     if m.Content == "ping" {
-        s.ChannelMessageSend(m.ChannelID, "pang")
+        s.ChannelMessageSend(m.ChannelID, "")
+        fmt.Printf("%+v\n", m.Author)
     }
 
     if m.Content == "hey" {
         s.ChannelMessageSend(m.ChannelID, "sup")
     }
+
+    if m.Content == "fish" {
+        bassType := "Large-mouth"
+        defaultMin := 20
+        defaultRange := 80
+        length := defaultMin + rand.Intn(defaultRange)
+        s.ChannelMessageSend(m.ChannelID, fmt.Sprint(m.Author.Username, " caught a ", length, "cm ", bassType, " bass!"))
+    }
 }
+
+// Bait types: fly fishing, lure - jig, lure - spoon, lure - spinner, lure -crankbait, lure - plug, powerbait - plain, powerbait - glitter, worm, minnow, crayfish, cricket, frog, offal
